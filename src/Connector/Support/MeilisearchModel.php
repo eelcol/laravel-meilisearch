c7fbb38b5c84af783327db5013cba5693041814e
Eelco <eelcoluurtsema@gmail.com>
2022-04-10 20:08:45 +0200

    Update MeilisearchModel to match ArrayAccess

--- src/Connector/Support/MeilisearchModel.php
+++ src/Connector/Support/MeilisearchModel.php
@@ -18,22 +18,22 @@ class MeilisearchModel implements ArrayAccess
         return $this->data;
     }
 
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         // cannot set
     }
 
-    public function offsetExists($offset)
+    public function offsetExists(mixed $offset): bool
     {
         return isset($this->data[$offset]);
     }
 
-    public function offsetUnset($offset)
+    public function offsetUnset(mixed $offset): void
     {
         // cannot unset
     }
 
-    public function offsetGet($offset)
+    public function offsetGet(mixed $offset): mixed
     {
         return $this->data[$offset] ?? null;
     }
